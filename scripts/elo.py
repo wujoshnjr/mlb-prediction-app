50a68b0b87d0fe39bec2950c8c5de90fa2ed21a0
wujoshnjr <a0905271182@gmail.com>
2026-05-18 14:15:13 +0800

    Update elo.py

--- scripts/elo.py
+++ scripts/elo.py
@@ -1,7 +1,3 @@
-"""
-MLB ELO 评分系统
-球队实力动态评分，用于捕捉状态起伏
-"""
 class MLBElosystem:
     def __init__(self, k_factor=32, home_advantage=24):
         self.elos = {}
@@ -12,16 +8,12 @@ class MLBElosystem:
         return 1 / (1 + 10 ** ((elo_b - elo_a) / 400))
 
     def update(self, home, away, home_win, date_str):
-        # 初始化新球队
         for team in [home, away]:
             if team not in self.elos:
                 self.elos[team] = 1500
-
         elo_home = self.elos[home] + self.home_adv
         elo_away = self.elos[away]
-        
         expected = self.expected_score(elo_home, elo_away)
         actual = 1 if home_win else 0
-        
         self.elos[home] += self.k * (actual - expected)
         self.elos[away] -= self.k * (actual - expected)
