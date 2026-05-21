42387974d0b57819d0922d75e91305c7fcdeabfa
wujoshnjr <a0905271182@gmail.com>
2026-05-21 18:12:59 +0800

    Update elo.py

--- scripts/elo.py
+++ scripts/elo.py
@@ -8,7 +8,6 @@ class MLBElosystem:
         self.k = k_factor
         self.home_adv = home_advantage
         self.elos = self._load()
-
     def _load(self):
         if os.path.exists(ELO_FILE):
             try:
@@ -17,15 +16,12 @@ class MLBElosystem:
             except:
                 pass
         return {}
-
     def save(self):
         os.makedirs("data", exist_ok=True)
         with open(ELO_FILE, 'w') as f:
             json.dump(self.elos, f, indent=2)
-
     def expected_score(self, elo_a, elo_b):
         return 1 / (1 + 10 ** ((elo_b - elo_a) / 400))
-
     def update(self, home, away, home_win, date_str=None):
         for team in [home, away]:
             if team not in self.elos:
