1dc17364a180e6282d4b7bfa0b2cc50400eb1f90
wujoshnjr <a0905271182@gmail.com>
2026-05-23 16:03:31 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+FEATURE_USE_PITCH_MATCHUP = True
+MODEL_META = 'elasticnet'          # 'lr' or 'elasticnet'
+MODEL_USE_MLP = True
+RATINGS_ENGINE = 'glicko2'         # 'elo' or 'glicko2'
+WALKFORWARD_STRICT = True
+NRFI_USE_ML = True
+ODDS_USE_CURVE_FEATURES = True
