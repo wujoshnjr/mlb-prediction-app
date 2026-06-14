e9204f7a1029ab8685e8a271b11d62c0b2cffbb4
wujoshnjr <a0905271182@gmail.com>
2026-06-14 11:36:18 +0800

    Update pipeline_manifest.py

--- scripts/pipeline_manifest.py
+++ scripts/pipeline_manifest.py
@@ -48,6 +48,7 @@ TRACKED_FILES = [
     "report/saas_readiness_report.json",
     "report/model_artifact_status_report.json",
     "report/model_status_consistency_report.json",
+    "report/artifact_rebuild_readiness_report.json",
     "report/sample_state_report.json",
     "report/training_samples_report.json",
     "report/outcome_linkage_diagnostic.json",
